fbbbdf81a4bcda9bb87a212dd6f1ac47ef7c2582
Ivan Hrabcak <ivan@hrabcak.eu>
2023-12-03 16:52:05 +0100

    A small refactor of day three

--- day_3/solution.js
+++ day_3/solution.js
@@ -4,10 +4,28 @@ const isDigit = (s) => s.charCodeAt(0) >= '0'.charCodeAt(0) && s.charCodeAt(0) <
 const isInBounds = (x, y, a) =>
     x < a.length && x >= 0 && y < a[0].length && y >= 0
 
-const partTwo = (lines) => {
-    for (let k = 0; k < lines.length; k++) {
-        lines[k] = '.' + lines[k] + '.'
+const shouldBeCounted = (i, j, lines, buf) => {
+    for (let x = i - 1; x <= i + 1; x++) {
+        if (x < 0 || x > lines.length) {
+            continue
+        }
+
+        for (let y = j - (buf.length + 1); y <= j; y++) {
+            if (!isInBounds(x,  y, lines)) {
+                continue
+            }
+
+            if (lines[x][y] != '.' && !isDigit(lines[x][y])) {
+                return true
+            }
+        }
     }
+
+    return false
+}
+
+const partTwo = (lines) => {
+    lines = lines.map(l => `.${l}.`)
 
     const gearNeighbors = {}
 
@@ -16,30 +34,8 @@ const partTwo = (lines) => {
         for (let j = 0; j < lines[0].length; j++) {
             if (isDigit(lines[i][j])) {
                 buf += lines[i][j]
-            } else if (buf != '') {
-                let shouldBeCounted = false
-                for (let x = i - 1; x <= i + 1; x++) {
-                    if (x < 0 || x > lines.length) {
-                        continue
-                    }
-
-                    if (shouldBeCounted) {
-                        break
-                    }
-
-                    for (let y = j - (buf.length + 1); y <= j; y++) {
-                        if (!isInBounds(x,  y, lines)) {
-                            continue
-                        }
-
-                        if (lines[x][y] != '.' && !isDigit(lines[x][y])) {
-                            shouldBeCounted = true
-                            break
-                        }
-                    }
-                }
-                
-                if (shouldBeCounted) {
+            } else if (buf != '') {               
+                if (shouldBeCounted(i, j, lines, buf)) {
                     for (let x = i - 1; x <= i + 1; x++) {
                         if (x < 0 || x > lines.length) {
                             continue
@@ -66,17 +62,15 @@ const partTwo = (lines) => {
         }
     } 
 
-    return Object.entries(gearNeighbors)
-        .filter(([k, v]) => v.length == 2)
-        .map(([k, v]) => v[0] * v[1])
+    return Object.values(gearNeighbors)
+        .filter((v) => v.length == 2)
+        .map((v) => v[0] * v[1])
         .reduce((acc, v) => acc + v, 0)
 
 }
 
 const partOne = (lines) => {
-    for (let k = 0; k < lines.length; k++) {
-        lines[k] = '.' + lines[k] + '.'
-    }
+    lines = lines.map(l => `.${l}.`)
     
     let sum = 0
 
@@ -86,29 +80,7 @@ const partOne = (lines) => {
             if (isDigit(lines[i][j])) {
                 buf += lines[i][j]
             } else if (buf != '') {
-                let shouldBeCounted = false
-                for (let x = i - 1; x <= i + 1; x++) {
-                    if (x < 0 || x > lines.length) {
-                        continue
-                    }
-
-                    if (shouldBeCounted) {
-                        break
-                    }
-
-                    for (let y = j - (buf.length + 1); y <= j; y++) {
-                        if (!isInBounds(x,  y, lines)) {
-                            continue
-                        }
-
-                        if (lines[x][y] != '.' && !isDigit(lines[x][y])) {
-                            shouldBeCounted = true
-                            break
-                        }
-                    }
-                }
-                
-                if (shouldBeCounted) {
+                if (shouldBeCounted(i, j, lines, buf)) {
                     sum += parseInt(buf)
                 }
                 buf = ''
