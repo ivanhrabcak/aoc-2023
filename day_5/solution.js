33e8a6b3d3c8f78f43ab34b2fd69293e929a848e
Ivan Hrabcak <ivan@hrabcak.eu>
2023-12-05 22:48:46 +0100

    Add a non-working solution

--- day_5/solution.js
+++ day_5/solution.js
@@ -14,6 +14,39 @@ const intervalIntersection = (a, b) => {
     ]
 }
 
+const intervalDifference = (interval1, interval2) => {
+    const result = [];
+
+    if (interval1[1] < interval2[0] || interval2[1] < interval1[0]) {
+        return [interval1, interval2];
+    }
+
+    if (interval1[0] < interval2[0]) {
+        result.push([interval1[0], interval2[0] - 1]);
+    }
+
+    if (interval1[1] > interval2[1]) {
+        result.push([interval2[1] + 1, interval1[1]]);
+    }
+
+    return result;
+}
+
+const mergeIntervals = (intervals) => {
+    intervals.sort((a, b) => a[0] - b[0])
+    let result = [[intervals[0][0], intervals[0][1]]]
+    
+    intervals.forEach((interval, i, arr) => {
+        if (interval[0] <= result[result.length - 1][1] + 1) {
+            result[result.length - 1][1] = Math.max(result[result.length - 1][1], interval[1])
+        } else {
+            result.push(interval)
+        }
+    })
+
+    return result
+}
+
 const partOne = (seeds, conversions) => {
     return Math.min(
         ...conversions.reduce((acc, conversion) => {
@@ -40,27 +73,37 @@ const partOne = (seeds, conversions) => {
 const partTwo = (seeds, conversions) => {
     return Math.min(
         ...conversions.reduce((acc, conversion) => {
-            return acc.map((seedRange) => {
-                const intersectionResult = conversion.ranges
-                    .map(([_, srcStart, size], i) => [[srcStart, srcStart + size], i])
-                    .map(([interval, i]) => [intervalIntersection(seedRange, interval), i])
-                    .filter(([interval, _]) => interval !== null)
-                
-                if (intersectionResult.length != 0) {
-                    const [_, rangeIndex] = intersectionResult[0]
+            return mergeIntervals(
+                acc.flatMap((seedRange) => {
+                    const intersectionResult = conversion.ranges
+                        .map(([_, srcStart, size], i) => [[srcStart, srcStart + size], i])
+                        .map(([interval, i]) => [intervalIntersection(seedRange, interval), i])
+                        .filter(([interval, _]) => interval !== null)
+                    
+                    if (intersectionResult.length != 0) {
+                        const [intersection, rangeIndex] = intersectionResult[0]
 
-                    const [destStart, srcStart, size] = conversion.ranges[rangeIndex]
-                    return (seed - srcStart) + destStart
-                } else {
-                    return seed
-                }
-            })
-        }, seeds)
+                        const [destStart, srcStart, size] = conversion.ranges[rangeIndex]
+                        const rangeStart = (intersection[0] - srcStart) + destStart
+                        const rangeEnd = (intersection[1] - srcStart) + destStart
+
+                        const unmatchedRange = intervalDifference(seedRange, intersection)
+                        return [[rangeStart, rangeEnd], ...unmatchedRange]
+                    } else {
+                        return [seedRange]
+                    }
+                })
+            )
+        }, seeds.map((s, i, arr) => i % 2 == 1 ? [arr[i - 1], s] : [])
+            .filter(k => k.length !== 0)
+            .map(([start, size]) => [start, start + size])
+        )
+        .map(interval => interval[0])
     )
 }
 
 const main = () => {
-    const input = fs.readFileSync('C:\\Users\\ivanh\\Repositories\\aoc-2023\\day_5\\input.txt').toString()
+    const input = fs.readFileSync('./input.txt').toString()
     const [seedList, ...conversionMaps] = input.split('\n\n')
 
     const seeds = seedList.split(': ')[1]
@@ -77,6 +120,7 @@ const main = () => {
     })
 
     console.log(partOne(seeds, conversions))
+    console.log(partTwo(seeds, conversions))
 }
 
 main()
